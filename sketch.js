a0d442e4939203879844d1bc59fa3e2575b14e82
Bekircan dal <bekircandal.amasya.05@gmail.com>
2018-06-14 18:17:57 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let x = 0,y = 0;
+
+let xCor = 0;
+let yCor = 0;
+
+
+function setup() {
+  createCanvas(1200,800);
+
+  background(0);
+  colorMode(HSB);
+}
+
+function draw() {
+
+
+  drawCircles();
+  drawCircles();
+  drawCircles();
+  drawCircles();
+
+}
+
+function drawCircles() {
+  stroke(x,255,255);
+  strokeWeight(5);
+  noFill();
+  xCor = random(0,width);
+  yCor = random(0,height);
+
+  ellipse(xCor,yCor,25,25);
+
+  if(x > 255){
+    x = 0;
+  }
+  x+=10;
+}
+
+function mouseDragged() {
+  stroke(x,255,255);
+  strokeWeight(5);
+  //noFill();
+  ellipse(mouseX,mouseY,25,25);
+
+  if(x > 255){
+    y+=50;
+    x = 0;
+  }
+
+  if(y > 255){
+    y = 50;
+  }
+  x+=10;
+
+}
