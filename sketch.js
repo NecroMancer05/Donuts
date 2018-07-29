34f54bf7ac06ad040e2ea386ba529beb1c364e30
Bekircan DAL <bekircandal.amasya.05@gmail.com>
2018-07-29 15:21:08 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,72 +1,40 @@
-let numbers = [];
-let count = 1;
-let sequence = [];
-let index = 0;
+let x = 0,y = 0;
+let xCor = 0;
+let yCor = 0;
 
-let arcs = [];
+ function setup() {
+  createCanvas(1200,800);
+   background(0);
+  colorMode(HSB);
+}
+ function draw() {
+   drawCircles();
+ }
 
-let biggest = 0;
-
-class Arc {
-  constructor(start, end, dir) {
-    this.start = start;
-    this.dir = dir;
-    this.end = end;
+ function drawCircles() {
+  stroke(x,255,255);
+  strokeWeight(5);
+  noFill();
+  xCor = random(0,width);
+  yCor = random(0,height);
+   ellipse(xCor,yCor,25,25);
+   if(x > 255){
+    x = 0;
   }
-
-  show(){
-    let diameter = abs(this.end - this.start);
-    let x = (this.end + this.start) /2;
-
-    stroke(255);
-    noFill();
-
-    if (this.dir == 0) {
-      arc(x, height / 2, diameter, diameter, 0, PI);
-    }else{
-      arc(x, height / 2, diameter, diameter, PI, 0);
-    }
-  }
+  x+=10;
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  numbers[index] = true;
-
-  sequence.push(index);
-}
-
-function step(){
-
-  let next = index - count;
-
-  if (next < 0 || numbers[next]) {
-
-    next = index + count;
+ function mouseDragged() {
+  stroke(x,255,255);
+  strokeWeight(5);
+  //noFill();
+  ellipse(mouseX,mouseY,25,25);
+   if(x > 255){
+    y+=50;
+    x = 0;
   }
-  numbers[next] = true;
-  sequence.push(next);
-
-
-  let a = new Arc(index, next, count % 2);
-  arcs.push(a);
-
-  index = next;
-
-  if (index > biggest) {
-
-    biggest = index
+   if(y > 255){
+    y = 50;
   }
-  count++;
-}
-
-function draw() {
-  step();
-  translate(0 , height / 2);
-  scale(width / biggest);
-  background(0);
-
-  for(let a of arcs){
-    a.show();
-  }
-}
+  x+=10;
+ }
